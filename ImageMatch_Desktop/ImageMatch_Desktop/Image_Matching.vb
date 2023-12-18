a5aa10c5d071a7adaa5b730d842c2df70e945f88
Enoch Taylor <tay121222@gmail.com>
2023-12-18 10:36:13 +0000

    Updated recognizer code to check number of Training images before executiing imagematch

--- ImageMatch_Desktop/ImageMatch_Desktop/Image_Matching.vb
+++ ImageMatch_Desktop/ImageMatch_Desktop/Image_Matching.vb
@@ -27,80 +27,85 @@ Public Class Image_Matching
     End Sub
 
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
-        PictureBox4.Visible = True
-        find_match_worker.RunWorkerAsync()
+        If jewelryImages.Count < 10 Then
+            MessageBox.Show("Currently don't have enough images in Training Set to achieve Image match Accuracy", "Add " & (10 - jewelryImages.Count) & " more Jewelry Items", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        Else
+            PictureBox4.Visible = True
+            find_match_worker.RunWorkerAsync()
+        End If
+
     End Sub
-
-    Private Sub find_match_worker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles find_match_worker.DoWork
 #Region "old code"
-        'Try
-        '    TextBox1.Text = ""
-        '    TextBox4.Text = ""
-        '    TextBox3.Text = ""
+    'Try
+    '    TextBox1.Text = ""
+    '    TextBox4.Text = ""
+    '    TextBox3.Text = ""
 
-        '    Dim imagelist As List(Of Image(Of Gray, Byte)) = New List(Of Image(Of Gray, Byte))()
-        '    Dim labellist As List(Of String) = New List(Of String)()
+    '    Dim imagelist As List(Of Image(Of Gray, Byte)) = New List(Of Image(Of Gray, Byte))()
+    '    Dim labellist As List(Of String) = New List(Of String)()
 
-        '    For Each pair As KeyValuePair(Of String, Byte()) In jewelryImages
-        '        Dim ImgStream As New IO.MemoryStream(CType(pair.Value, Byte()))
-        '        Dim tempimage As Bitmap = (New Bitmap(Image.FromStream(ImgStream)))
-        '        imagelist.Add(New Image(Of Gray, Byte)(tempimage))
-        '        labellist.Add(pair.Key)
-        '        tempimage.Dispose()
-        '        ImgStream.Close()
-        '        ImgStream.Dispose()
-        '    Next
+    '    For Each pair As KeyValuePair(Of String, Byte()) In jewelryImages
+    '        Dim ImgStream As New IO.MemoryStream(CType(pair.Value, Byte()))
+    '        Dim tempimage As Bitmap = (New Bitmap(Image.FromStream(ImgStream)))
+    '        imagelist.Add(New Image(Of Gray, Byte)(tempimage))
+    '        labellist.Add(pair.Key)
+    '        tempimage.Dispose()
+    '        ImgStream.Close()
+    '        ImgStream.Dispose()
+    '    Next
 
-        '    If imagelist.Count = 1 Then Exit Sub
-        '    Dim TermCrit As MCvTermCriteria = New MCvTermCriteria(imagelist.Count, 0.001)
-        '    Dim recognizer As EigenObjectRecognizer = New EigenObjectRecognizer(imagelist.ToArray(), labellist.ToArray(), 1500, TermCrit)
+    '    If imagelist.Count = 1 Then Exit Sub
+    '    Dim TermCrit As MCvTermCriteria = New MCvTermCriteria(imagelist.Count, 0.001)
+    '    Dim recognizer As EigenObjectRecognizer = New EigenObjectRecognizer(imagelist.ToArray(), labellist.ToArray(), 1500, TermCrit)
 
-        '    For i As Integer = 0 To imagelist.Count - 1
-        '        Dim recognitionValue As EigenObjectRecognizer.RecognitionResult = recognizer.Recognize(image_to_find_match)
-        '        Thread.Sleep(100)
-        '        If recognitionValue.Label = labellist(i) Then
-        '            MsgBox(1)
-        '            TextBox1.Text = TextBox1.Text & ";" & recognizer.Recognize(image_to_find_match).Label
-        '            TextBox3.Text = TextBox3.Text & ";" & recognizer.Recognize(image_to_find_match).Distance
+    '    For i As Integer = 0 To imagelist.Count - 1
+    '        Dim recognitionValue As EigenObjectRecognizer.RecognitionResult = recognizer.Recognize(image_to_find_match)
+    '        Thread.Sleep(100)
+    '        If recognitionValue.Label = labellist(i) Then
+    '            MsgBox(1)
+    '            TextBox1.Text = TextBox1.Text & ";" & recognizer.Recognize(image_to_find_match).Label
+    '            TextBox3.Text = TextBox3.Text & ";" & recognizer.Recognize(image_to_find_match).Distance
 
-        '            Dim foundimage As Image(Of Gray, Byte) = imagelist(recognizer.Recognize(image_to_find_match).Index)
-        '            Dim difference As Image(Of Gray, Byte) = foundimage.AbsDiff(image_to_find_match)
+    '            Dim foundimage As Image(Of Gray, Byte) = imagelist(recognizer.Recognize(image_to_find_match).Index)
+    '            Dim difference As Image(Of Gray, Byte) = foundimage.AbsDiff(image_to_find_match)
 
-        '            Dim sum As Double = 0
+    '            Dim sum As Double = 0
 
-        '            For t As Integer = 0 To difference.Height - 1
-        '                For j As Integer = 0 To difference.Width - 1
-        '                    sum += difference(t, j).Intensity
-        '                Next
-        '            Next
+    '            For t As Integer = 0 To difference.Height - 1
+    '                For j As Integer = 0 To difference.Width - 1
+    '                    sum += difference(t, j).Intensity
+    '                Next
+    '            Next
 
-        '            Dim totalPixels As Double = difference.Height * difference.Width
-        '            Dim percentageMatch As Double = 100 - (sum / totalPixels * 100)
+    '            Dim totalPixels As Double = difference.Height * difference.Width
+    '            Dim percentageMatch As Double = 100 - (sum / totalPixels * 100)
 
-        '            e.Result = percentageMatch
+    '            e.Result = percentageMatch
 
-        '            Dim percentageMatch2 As Double = DirectCast(e.Result, Double)
+    '            Dim percentageMatch2 As Double = DirectCast(e.Result, Double)
 
-        '            PictureBox2.Image = imagelist(recognizer.Recognize(image_to_find_match).Index).ToBitmap
-        '            Label5.Text = percentageMatch2 & "%"
+    '            PictureBox2.Image = imagelist(recognizer.Recognize(image_to_find_match).Index).ToBitmap
+    '            Label5.Text = percentageMatch2 & "%"
 
-        '            'imagelist.RemoveAt(i)
-        '            'labellist.RemoveAt(i)
-        '            'Exit For
+    '            'imagelist.RemoveAt(i)
+    '            'labellist.RemoveAt(i)
+    '            'Exit For
 
-        '            imagelist.RemoveAt(recognitionValue.Index)
-        '            labellist.RemoveAt(recognitionValue.Index)
-        '        Else
+    '            imagelist.RemoveAt(recognitionValue.Index)
+    '            labellist.RemoveAt(recognitionValue.Index)
+    '        Else
 
-        '        End If
-        '    Next
+    '        End If
+    '    Next
 
-        '    Thread.Sleep(10)
+    '    Thread.Sleep(10)
 
-        'Catch ex As System.NullReferenceException
-        '    MessageBox.Show("No Object Detected", "", MessageBoxButtons.OK)
-        'End Try
+    'Catch ex As System.NullReferenceException
+    '    MessageBox.Show("No Object Detected", "", MessageBoxButtons.OK)
+    'End Try
 #End Region
+    Private Sub find_match_worker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles find_match_worker.DoWork
+
         image_to_find_match = New Image(Of Gray, Byte)(ResizeImage(New Bitmap(jewelry_image.Image)))
         Try
 
